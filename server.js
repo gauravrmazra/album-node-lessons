ebc500623277041c5b76518adc61b42af47944b5
Gaurav Mazra <gaurav.r.mazra@gmail.com>
2017-07-23 23:09:31 -0400

    logged server start time

--- server.js
+++ server.js
@@ -20,3 +20,4 @@ function handle_incoming_requests(req, res) {
 
 var json_server = http.createServer(handle_incoming_requests);
 json_server.listen(8080);
+console.log("Server started at port: 8080");
