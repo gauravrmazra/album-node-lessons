209b3dc26e81ba4f6412e0b73872b104166d90c1
Gaurav Mazra <gaurav.r.mazra@gmail.com>
2017-07-23 23:29:44 -0400

    Used literals while logging application start message

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var fs = require("fs");
 var AlbumService = require("./service/AlbumService.js");
 const albumService = new AlbumService(fs);
+const app_port = 8080;
 
 function handle_incoming_requests(req, res) {
   console.log("INCOMING REQUEST: METHOD: " + req.method + " and URL: " + req.url);
@@ -19,5 +20,5 @@ function handle_incoming_requests(req, res) {
 }
 
 var json_server = http.createServer(handle_incoming_requests);
-json_server.listen(8080);
-console.log("Server started at port: 8080");
+json_server.listen(app_port);
+console.log(`Server started at port: ${app_port}`);
