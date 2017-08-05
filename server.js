d417eb929fb90c8c490c3db1c74253fbe192d638
Gaurav Mazra <gaurav.r.mazra@gmail.com>
2017-08-04 23:07:13 -0400

    Added support for getting photos from album

--- server.js
+++ server.js
@@ -6,17 +6,48 @@ const app_port = 8080;
 
 function handle_incoming_requests(req, res) {
   console.log("INCOMING REQUEST: METHOD: " + req.method + " and URL: " + req.url);
-  albumService.listAlbums((err, albums) => {
-    if (err) {
-      res.writeHead(200, {"Content-Type" : "application/json"});
-      res.end(JSON.stringify({ error: { code : "not able to read albums", message : err.message }, data: {}}));
-    }
-    else {
-      res.writeHead(200, {"Content-Type" : "application/json"});
-      var output = {error: null, data: {albums : albums}};
-      res.end(JSON.stringify(output));
-    }
-  });
+  if (req.url == '/albums.json') {
+    albumService.listAlbums((err, albums) => {
+      if (err) {
+        send_failure(res, 500, err);
+      }
+      else {
+        send_success(res, {albums : albums});
+      }
+    });
+  }
+  else if (req.url.substr(0, 7) == '/albums' && req.url.substr(req.url.length - 5) === '.json') {
+    var album_name = req.url.substr(7, req.url.length - 12);
+    albumService.getAlbumContents(album_name, (err, photos) => {
+      if (err) {
+        send_failure(res, 500, err);
+      }
+      else {
+        send_success(res, photos);
+      }
+    });
+  }
+  else {
+    send_failure(res, 404, {code : 'No page', message : 'No page'});
+  }
+}
+
+function error(code, message) {
+  var err = new Error(message);
+  err.code = code;
+  return err;
+}
+
+function send_success(response,  data) {
+  response.writeHead(200, {"Content-Type" : "application/json"});
+  var output = {error: null, data: data};
+  response.end(JSON.stringify(output));
+}
+
+function send_failure(response, server_code, err) {
+  var code = (err.code) ? err.code : err.name;
+  response.writeHead(server_code, {"Content-Type" : "application/json"});
+  response.end(JSON.stringify({code : (err.code) ? err.code : err.name, message: err.message}));
 }
 
 var json_server = http.createServer(handle_incoming_requests);
--- service/AlbumService.js
+++ service/AlbumService.js
@@ -10,8 +10,59 @@ module.exports = class AlbumService {
         callback(err);
       }
       else {
-        callback(null, files);
+        var only_dirs = [];
+
+        var iterator = (index) => {
+            if (index == files.length) {
+              callback(null, only_dirs);
+              return;
+            }
+
+            this.fs.stat("albums/" + files[index], (err, stats) => {
+              if (err) {
+                callback(err);
+                return;
+              }
+
+              if (stats.isDirectory()) {
+                only_dirs.push({ name : files[index]});
+              }
+              iterator(index + 1);
+            });
+        };
+        iterator(0);
       }
+    });
+  }
+
+  getAlbumContents(album_name, callback) {
+    this.fs.readdir("albums/" + album_name, (err, files) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      var only_files = [];
+      var path = `albums/${album_name}/`;
+
+      var iterator = (index) => {
+        if (index == files.length) {
+          callback(null, { short_name: album_name, photos: only_files });
+          return;
+        }
+
+        this.fs.stat(path + files[index], (err, stats) => {
+          if (err) {
+            callback(err);
+            return;
+          }
+          if (stats.isFile()) {
+            only_files.push({ filename: files[index], desc: files[index] });
+          }
+          iterator(index + 1)
+        });
+      };
+      iterator(0);
     });
   }
 }
